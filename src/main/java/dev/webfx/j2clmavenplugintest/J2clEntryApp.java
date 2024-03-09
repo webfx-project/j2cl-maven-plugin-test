b5582083f2b5bfe86937c1ca198581b72241dac3
Bruno Salmon <dev.salmonb@gmail.com>
2024-03-09 21:32:48 +0000

    Initial commit

---
+++ src/main/java/dev/webfx/j2clmavenplugintest/J2clEntryApp.java
@@ -0,0 +1,14 @@
+package dev.webfx.j2clmavenplugintest;
+
+import elemental2.dom.DomGlobal;
+
+/**
+ * @author Bruno Salmon
+ */
+public class J2clEntryApp {
+
+    public void entryPoint() {
+        DomGlobal.console.log("Hello world!");
+    }
+
+}
